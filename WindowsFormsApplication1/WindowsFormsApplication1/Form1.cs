180bcdf500f41c22192f6c601f2be2b52704a7b9
evilaction260895 <sergey_m10@mail.ru>
2016-11-18 13:31:46 +0300

    Rename some elements and functional

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -25,5 +25,14 @@ namespace WindowsFormsApplication1
             c = a + b;
             tP1_textBox3.Text = c.ToString();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a, b, c;
+            a = Convert.ToInt32(tP1_textBox1.Text);
+            b = Convert.ToInt32(tP1_textBox2.Text);
+            c = a + b;
+            tP1_textBox3.Text = c.ToString();
+        }
     }
 }
